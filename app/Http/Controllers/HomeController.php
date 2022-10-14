f63b064472d83df5ca4dd6626f76b922f3de5c05
Anju <anjubabu8714@gmail.com>
2022-10-14 12:50:41 +0530

    controller chnage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -14,4 +14,21 @@ class HomeController extends Controller
     {
         return view('contact');
     }
+    public function contactFormSubmit(Request $request){
+
+       
+
+        $this->validate($request,[
+            'email'=>'required',
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255'
+         ]);
+
+         dd($request->email);
+
+      
+        dd($request->first_name);
+
+        dd($request->last_name);
+    }
 }
